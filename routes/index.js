9804d0b28030acfaba9f98c0a25a21ea3759e1b1
mattmuroya <matt.muroya@gmail.com>
2022-03-25 23:33:29 -1000

    add index routes and views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello, World!",
+    user: "Charles",
+    added: new Date(),
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home', messages });
+});
+
+// get NEW MESSAGE page 
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'New Message' });
+});
+
+// post NEW MESSAGE
+router.post('/new', (req, res, next) => {
+  messages.unshift({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  })
+  res.redirect('/')
 });
 
 module.exports = router;
